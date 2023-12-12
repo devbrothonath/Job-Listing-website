a893c10cf282e9874c71df0428631d670727c0b0
Devbrotho Nath <devbrotho.dn@gmail.com>
2023-12-12 16:03:03 +0530

    added database config and initialzation

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,41 @@
 const express = require("express");
+const { default: mongoose } = require("mongoose");
 require("dotenv").config();
 
 const app = express();
 
 const PORT = process.env.PORT;
+const DB_URI = process.env.MONGO_URI;
+
+mongoose
+  .connect(DB_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`server listening at ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 app.get("/", (req, res) => {
-    res.send("Hello from backend")
-})
+  res.send("Hello from backend");
+});
 
-app.listen(PORT, () => {
-    console.log(`server listening at ${PORT}`)
-})
+let serverState = true;
+app.get("/health", (req, res) => {
+  const currentTime = new Date().toLocaleTimeString();
+  if (serverState) {
+    res.status(200).json({
+      servername: "job-listing-server",
+      serverStatus: "active",
+      time: currentTime,
+    });
+  } else {
+    res.status(500).json({
+      servername: "job-listing-server",
+      serverStatus: "inactive",
+      time: currentTime,
+    });
+  }
+});
